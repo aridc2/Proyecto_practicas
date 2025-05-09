1fb07edab016782f7c486d1e67502880431fa54a
aridc2 <ariadnedia.2024.alumn@cescristorey.com>
2025-05-09 12:52:27 +0200

    Index con un caso de prueba para las dos clases

---
+++ index.py
@@ -0,0 +1,12 @@
+#Recuperamos el Entorno
+from clases import *
+
+
+empresa1 = Empresa("A12345678", "Mi Empresa S.A.","Sociedad Anónima", "Tecnología", "Granada", "123456789" )
+print(empresa1)
+
+p_1= Proyecto ("A12345678","Mi Empresa S.A.",1,'Proyecto 1','Activo','Si','Pepe','2','4')
+print(p_1)
+
+
+
