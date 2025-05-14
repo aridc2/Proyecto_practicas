4631776b5963d602bc39549d607b988bde7dee63
aridc2 <ariadnedia.2024.alumn@cescristorey.com>
2025-05-14 13:04:08 +0200

    Cambios en la ventana de actualizacion de informacion de empresa para hacerla mas intuitiva

--- interfaz.py
+++ interfaz.py
@@ -3,9 +3,13 @@ from tkinter import messagebox, ttk
 import mysql.connector as mysql
 from clases import *
 
+
 ###################################################
 # ~~~~~~~~~~~~~~~~~~~EMPRESAS~~~~~~~~~~~~~~~~~~~~ #
 ###################################################
+tipos_sociedad = Empresa.tipos_sociedad
+sectores = Empresa.sectores
+
 
 def ventana_inserccion_empresa():
     root.withdraw()
@@ -53,20 +57,6 @@ def ventana_inserccion_empresa():
     def cerrar_e_ir_menu():
         insertar_win.destroy()
         root.deiconify()
-
-    tipos_sociedad = [
-    "Sociedad Anónima", "Sociedad Limitada", "Sociedad Cooperativa",
-    "Sociedad Comanditaria", "Sociedad Civil", "Sociedad autonoma", "Sociedad colecctiva"
-    ]
-
-    sectores = [
-    "Agricultura", "Pesca", "Minería", "Silvicultura", "Industria alimentaria",
-    "Construcción", "Metalurgia", "Automoción", "Química", "Textil", "Manufactura",
-    "Comercio", "Transporte y logística", "Hostelería y turismo", "Educación", "Sanidad",
-    "Servicios financieros", "Consuloría", "Tecnología", "Software",
-    "Consultoría especializada", "Startups tecnológicas", "ONG", "Asistencia social",
-    "Servicios culturales", "Formación personal"
-    ]
 
     titulo = tk.Label(insertar_win, text="Insertar Empresa", font=("Helvetica", 20, "bold"), bg="#eaf2f8", fg="#2e4053")
     titulo.pack(pady=20)
@@ -196,14 +186,9 @@ def ventana_actualizar_empresa():
     actualizar_win.title('Gestor de proyectos')
     actualizar_win.config(bg="#eaf2f8")
 
-    def update():
+    def buscar():
         CIF = CIF_entry.get()
-        nombre_empresa = nombre_empresa_entry.get()
-        tipo_sociedad = tipo_sociedad_entry.get()
-        Sector = Sector_entry.get()
-        Localidad = Localidad_entry.get()
-        telefono_empresa = telefono_empresa_entry.get()
-
+        
         if CIF == '':
             tk.messagebox.showinfo('ALERT', 'Por favor introduzca el CIF para poder actualizar los datos')
         else:
@@ -215,44 +200,15 @@ def ventana_actualizar_empresa():
                     database='gestionProyectos'
                 )
                 cursor = con.cursor()
-                
-                if nombre_empresa!= '':
-                    sql ='UPDATE Empresa SET Nombre = %s WHERE CIF = %s'
-                    valores = (nombre_empresa,CIF)
-                    cursor.execute(sql, valores)
-
-                if tipo_sociedad!= '':
-                    sql ='UPDATE Empresa SET Tipo_sociedad = %s WHERE CIF = %s'
-                    valores = (tipo_sociedad,CIF)
-                    cursor.execute(sql, valores)
-
-                if Sector!= '':
-                    sql ='UPDATE Empresa SET Sector = %s WHERE CIF = %s'
-                    valores = (Sector,CIF)
-                    cursor.execute(sql, valores)
-
-                if Localidad!= '':
-                    sql ='UPDATE Empresa SET Localidad = %s WHERE CIF = %s'
-                    valores = (Localidad,CIF)
-                    cursor.execute(sql, valores)
-
-                if telefono_empresa!= '':
-                    sql ='UPDATE Empresa SET N_Telefono = %s WHERE CIF = %s'
-                    valores = (telefono_empresa,CIF)
-                    cursor.execute(sql, valores)
-
-                con.commit()
-                if cursor.rowcount == 0:
-                    tk.messageBox.showinfo('Resultado', 'No se encontró ninguna empresa con ese CIF')
+                cursor.execute("SELECT 1 FROM Empresa WHERE CIF = %s LIMIT 1", (CIF,))
+                resultado = cursor.fetchone()
+                if resultado == None:
+                    tk.messagebox.showinfo('Resultado', 'No se encontró ninguna empresa con ese CIF')
                 else:
-                    tk.messagebox.showinfo('Status', 'Datos actualizados correctamente')
+                    ventana_rellenar_campos(CIF)
 
                     CIF_entry.delete(0, 'end')
-                    nombre_empresa_entry.delete(0, 'end')
-                    tipo_sociedad_entry.delete(0, 'end')
-                    Sector_entry.delete(0, 'end')
-                    Localidad_entry.delete(0, 'end')
-                    telefono_empresa_entry.delete(0, 'end')
+                    
 
                 con.close()
             except Exception as e:
@@ -262,7 +218,125 @@ def ventana_actualizar_empresa():
         actualizar_win.destroy()
         root.deiconify()
 
-    
+    def ventana_rellenar_campos(CIF):
+        CIF_buscado=CIF
+        actualizar_win.withdraw()
+        actualizar_campos_win = tk.Toplevel()
+        actualizar_campos_win.geometry('800x500')
+        actualizar_campos_win.title('Gestor de proyectos')
+        actualizar_campos_win.config(bg="#eaf2f8")
+
+        def cerrar_e_ir_menu():
+            actualizar_campos_win.destroy()
+            actualizar_win.deiconify()
+        def update(CIF_buscado):
+
+            nombre_empresa = nombre_empresa_entry.get()
+            tipo_sociedad = tipo_sociedad_combo.get()
+            Sector = Sector_combo.get()
+            Localidad = Localidad_entry.get()
+            telefono_empresa = telefono_empresa_entry.get()
+
+            
+            try:
+                con = mysql.connect(
+                    host='localhost',
+                    user='root',
+                    password='josegras',
+                    database='gestionProyectos'
+                    )
+                cursor = con.cursor()
+                    
+                if nombre_empresa!= '':
+                    sql ='UPDATE Empresa SET Nombre = %s WHERE CIF = %s'
+                    valores = (nombre_empresa,CIF_buscado)
+                    cursor.execute(sql, valores)
+
+                if tipo_sociedad!= '':
+                    sql ='UPDATE Empresa SET Tipo_sociedad = %s WHERE CIF = %s'
+                    valores = (tipo_sociedad,CIF_buscado)
+                    cursor.execute(sql, valores)
+
+                if Sector!= '':
+                    sql ='UPDATE Empresa SET Sector = %s WHERE CIF = %s'
+                    valores = (Sector,CIF_buscado)
+                    cursor.execute(sql, valores)
+
+                if Localidad!= '':
+                    sql ='UPDATE Empresa SET Localidad = %s WHERE CIF = %s'
+                    valores = (Localidad,CIF_buscado)
+                    cursor.execute(sql, valores)
+
+                if telefono_empresa!= '':
+                    sql ='UPDATE Empresa SET N_Telefono = %s WHERE CIF = %s'
+                    valores = (telefono_empresa,CIF_buscado)
+                    cursor.execute(sql, valores)
+
+                con.commit()
+                if cursor.rowcount == 0:
+                    tk.messagebox.showinfo('Resultado', 'No se encontró ninguna empresa con ese CIF')
+                else:
+                    tk.messagebox.showinfo('Status', 'Datos actualizados correctamente')
+
+                    
+                    nombre_empresa_entry.delete(0, 'end')
+                    tipo_sociedad_combo.delete(0, 'end')
+                    Sector_combo.delete(0, 'end')
+                    Localidad_entry.delete(0, 'end')
+                    telefono_empresa_entry.delete(0, 'end')
+
+                con.close()
+            except Exception as e:
+                tk.messagebox.showinfo('Error',str(e))
+
+        titulo = tk.Label(actualizar_campos_win, text="Actualizar Empresa", font=("Helvetica", 20, "bold"), bg="#eaf2f8", fg="#2e4053")
+        titulo.pack(pady=20)
+
+        
+        form_frame = tk.Frame(actualizar_campos_win, bg="#eaf2f8")
+        form_frame.pack(pady=10)
+
+        label_style = {"font": ("Helvetica", 12), "bg": "#eaf2f8", "fg": "#2e4053"}
+        entry_style = {"font": ("Helvetica", 12), "width": 30}
+
+
+        tk.Label(form_frame, text="Nombre empresa:", **label_style).grid(row=1, column=0, sticky="e", padx=10, pady=10)
+        nombre_empresa_entry = tk.Entry(form_frame, **entry_style)
+        nombre_empresa_entry.grid(row=1, column=1)
+
+        tk.Label(form_frame, text="Tipo de sociedad:", **label_style).grid(row=2, column=0, sticky="e", padx=10, pady=10)
+        tipo_sociedad_combo = ttk.Combobox(form_frame, values=tipos_sociedad, font=("Helvetica", 12), state="readonly", width=28)
+        tipo_sociedad_combo.grid(row=2, column=1)
+
+        tk.Label(form_frame, text="Sector:", **label_style).grid(row=3, column=0, sticky="e", padx=10, pady=10)
+        Sector_combo = ttk.Combobox(form_frame, values=sectores, font=("Helvetica", 12), state="readonly", width=28)
+        Sector_combo.grid(row=3, column=1)
+
+        tk.Label(form_frame, text="Localidad:", **label_style).grid(row=4, column=0, sticky="e", padx=10, pady=10)
+        Localidad_entry = tk.Entry(form_frame, **entry_style)
+        Localidad_entry.grid(row=4, column=1)
+
+        tk.Label(form_frame, text="Teléfono empresa:", **label_style).grid(row=5, column=0, sticky="e", padx=10, pady=10)
+        telefono_empresa_entry = tk.Entry(form_frame, **entry_style)
+        telefono_empresa_entry.grid(row=5, column=1)
+
+        btn_style = {
+            "font": ("Helvetica", 12, "bold"),
+            "width": 15,
+            "height": 1,
+            "bg": "#3498db",
+            "fg": "white",
+            "bd": 0,
+            "activebackground": "#2980b9",
+            "cursor": "hand2"
+        }
+
+        button_frame = tk.Frame(actualizar_campos_win, bg="#eaf2f8")
+        button_frame.pack(pady=20)
+
+        tk.Button(button_frame, text="Actualizar", command=lambda:update(CIF_buscado), **btn_style).grid(row=0, column=0, padx=10)
+        tk.Button(button_frame, text="Volver", command=cerrar_e_ir_menu, **btn_style).grid(row=0, column=1, padx=10)
+
     titulo = tk.Label(actualizar_win, text="Actualizar Empresa", font=("Helvetica", 20, "bold"), bg="#eaf2f8", fg="#2e4053")
     titulo.pack(pady=20)
 
@@ -276,26 +350,6 @@ def ventana_actualizar_empresa():
     tk.Label(form_frame, text="CIF:", **label_style).grid(row=0, column=0, sticky="e", padx=10, pady=10)
     CIF_entry = tk.Entry(form_frame, **entry_style)
     CIF_entry.grid(row=0, column=1)
-
-    tk.Label(form_frame, text="Nombre empresa:", **label_style).grid(row=1, column=0, sticky="e", padx=10, pady=10)
-    nombre_empresa_entry = tk.Entry(form_frame, **entry_style)
-    nombre_empresa_entry.grid(row=1, column=1)
-
-    tk.Label(form_frame, text="Tipo de sociedad:", **label_style).grid(row=2, column=0, sticky="e", padx=10, pady=10)
-    tipo_sociedad_entry = tk.Entry(form_frame, **entry_style)
-    tipo_sociedad_entry.grid(row=2, column=1)
-
-    tk.Label(form_frame, text="Sector:", **label_style).grid(row=3, column=0, sticky="e", padx=10, pady=10)
-    Sector_entry = tk.Entry(form_frame, **entry_style)
-    Sector_entry.grid(row=3, column=1)
-
-    tk.Label(form_frame, text="Localidad:", **label_style).grid(row=4, column=0, sticky="e", padx=10, pady=10)
-    Localidad_entry = tk.Entry(form_frame, **entry_style)
-    Localidad_entry.grid(row=4, column=1)
-
-    tk.Label(form_frame, text="Teléfono empresa:", **label_style).grid(row=5, column=0, sticky="e", padx=10, pady=10)
-    telefono_empresa_entry = tk.Entry(form_frame, **entry_style)
-    telefono_empresa_entry.grid(row=5, column=1)
 
     btn_style = {
         "font": ("Helvetica", 12, "bold"),
@@ -311,7 +365,7 @@ def ventana_actualizar_empresa():
     button_frame = tk.Frame(actualizar_win, bg="#eaf2f8")
     button_frame.pack(pady=20)
 
-    tk.Button(button_frame, text="Actualizar", command=update, **btn_style).grid(row=0, column=0, padx=10)
+    tk.Button(button_frame, text="Buscar", command=buscar, **btn_style).grid(row=0, column=0, padx=10)
     tk.Button(button_frame, text="Volver", command=cerrar_e_ir_menu, **btn_style).grid(row=0, column=1, padx=10)
 
     actualizar_win.mainloop()
